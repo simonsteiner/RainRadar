c6916a27d42dd3a72798d0f4b82ac6133e9f41e9
Simon Steiner <simonsteiner@users.noreply.github.com>
2024-12-08 13:32:28 +0100

    refactor: update stylelint configuration for improved selector patterns and ID usage

--- app/stylelint.config.js
+++ app/stylelint.config.js
@@ -7,10 +7,9 @@ module.exports = {
     "stylelint-order"
   ],
   rules: {
-    "selector-class-pattern": "^[a-z][a-zA-Z0-9]+$",
     "declaration-block-no-duplicate-properties": true,
     "declaration-no-important": true,
-    "selector-max-id": 0,
+    "selector-max-id": 1,
     "selector-max-universal": 1,
     "max-nesting-depth": 3,
     "shorthand-property-no-redundant-values": true,
@@ -20,6 +19,8 @@ module.exports = {
     "color-hex-length": "short",
     "color-named": "never",
     "selector-no-qualifying-type": true,
-    "declaration-block-single-line-max-declarations": 1
+    "declaration-block-single-line-max-declarations": 1,
+    "selector-class-pattern": "^([a-z][a-z0-9]*)(-[a-z0-9]+)*$",
+    "selector-id-pattern": "^[a-z][a-zA-Z0-9]+$"
   }
 };
